4faa6625834a2306980dd34748ff7ef7cae24db4
Lauti <73679349+LautaroBernati@users.noreply.github.com>
2023-09-24 19:21:59 -0300

    traduzco cv

--- src/app/cv/ui/cv-items/cv-items.module.ts
+++ src/app/cv/ui/cv-items/cv-items.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatIconModule,
     MatButtonModule,
+    TranslateModule.forChild(),
   ],
   exports: [CVItemComponent],
   providers: [],
--- src/app/cv/ui/lang-item-ui/lang.component.ts
+++ src/app/cv/ui/lang-item-ui/lang.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Language } from '../../data-access/languages.service';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'cv-lang-item-ui',
   templateUrl: 'lang.component.html',
   styleUrls: ['lang.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, TranslateModule]
 })
 export class LanguageItemComponent {
   @Input('item') public item: Language | null = null;
-
 }
